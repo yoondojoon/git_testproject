01e2407bdf57742ea7e73731f6a30d0778043e5f
yoondojoon <blueyerr12@naver.com>
2024-02-13 10:07:23 +0900

    Initial commit

---
+++ src/main/java/kr/or/iei/GitTestprojectApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestprojectApplication.class, args);
+	}
+
+}
